f54d32a5270940dd7fffba067922c616438ec0aa
BreadcrumbzX <seanwyu@gmail.com>
2017-03-13 15:56:29 -0400

    dfadsfsa

--- app/src/main/java/com/memeteam/sean/greatjob/MainActivity.java
+++ app/src/main/java/com/memeteam/sean/greatjob/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-hello shekmonstasa
+hello shekmonstas, hi sean
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
